161fb8dc8d730fcaa927c94e66c916f32753dc85
armandoomtzz <armandoomtzz@gmail.com>
2016-11-21 22:36:06 -0600

    Creacion de proyectos WebApi y WebMvc.

---
+++ RentDrive/WebApiRentDrive/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiRentDrive.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RentDrive/WebMvcRentDrive/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebMvcRentDrive.MvcApplication" Language="C#" %>
